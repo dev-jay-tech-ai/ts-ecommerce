18efe932e6cb612c6c692af9a062f27b3f4dfe1a
jaewonks <jaewonaaart@gmail.com>
2024-02-22 14:31:02 +0000

    Create react context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,34 @@
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { useContext, useEffect } from 'react'
+import { Button, Container, Nav, Navbar } from 'react-bootstrap'
 import { Outlet } from 'react-router-dom'
+import { Store } from './Store'
 
 function App() {
+  const { state: { mode }, dispatch } = useContext(Store)
+
+  useEffect(() => {
+    document.body.setAttribute('data-bs-theme', mode)
+  }, [mode])
+
+  const switchModeHandler = () => {
+    dispatch({ type: 'SWITCH_MODE' })
+  }
 
   return (
     <>
       <div className='d-flex flex-column vh-100'>
         <header>
-          <Navbar bg='dark' variant='dark' expand='lg'>
+          <Navbar expand='lg'>
             <Container>
               <Navbar.Brand>TS Amazona</Navbar.Brand>
+              <Nav>
+                <Button variant={mode} onClick={switchModeHandler}>
+                  <i className={mode === 'ligth' ? 'fa fa-sun' : 'fa fa-moon' }></i>
+                </Button>
+                <a href='/cart' className='nav-link'>Cart</a>
+                <a href='/signin' className='nav-link'>Sign In</a>
+              </Nav>
             </Container>
-            <Nav>
-              <a href='/cart' className='nav-link'>Cart</a>
-              <a href='/signin' className='nav-link'>Sign In</a>
-            </Nav>
           </Navbar>
         </header>
         <main>
