c83097298af6617a1dcde8f0d9e0da30f79b0271
jaewonks <jaewonaaart@gmail.com>
2024-02-22 11:32:56 +0000

    Create rating compoents

---
+++ frontend/src/components/ProductItem.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { Product } from "../types/Product";
+import { Button, Card } from "react-bootstrap";
+import Rating from "./Rating";
+
+function ProductItem({ product } : { product: Product }) {
+  return (
+    <Card>
+      <Link to={'/product/' + product.slug}>
+        <img className='product-image' src={product.image} alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link to={'/product/' + product.slug}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
+        <Card.Text>£{product.rating}</Card.Text>
+        {product.countInStock === 0 ? (
+          <Button variant='light' disabled >Out of stock</Button>
+        ) : (
+          <Button>Add to cart</Button>
+        )}
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default ProductItem
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import { Col, Row } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
 import { Product } from '../types/Product'
 import { useEffect, useReducer } from 'react'
 import axios from 'axios'
@@ -7,6 +6,7 @@ import { getError } from '../utils'
 import { ApiError } from '../types/ApiError'
 import LoadingBox from '../components/LoadingBox'
 import MessageBox from '../components/MessageBox'
+import ProductItem from '../components/ProductItem'
 
 type State = {
   product: Product[],
@@ -61,11 +61,7 @@ export default function HomePage() {
     {
       products.map((product) => 
       <Col key={product.slug} sm={6} md={4} lg={3}>
-        <Link to={'/product/' + product.slug}>
-          <img className='product-image' src={product.image} alt={product.name} />
-          <h2>{product.name}</h2>
-          <p>£{product.price}</p>
-        </Link>
+        <ProductItem product={product} />
       </Col>)
     }
     </Row>
