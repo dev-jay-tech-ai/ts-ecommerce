b846422f9ae24a6dcbc61067a4e29930a4415ef6
jaewonks <jaewonaaart@gmail.com>
2024-02-22 19:44:52 +0000

    Add to cart in product page

--- backend/src/routers/productRouter.ts
+++ backend/src/routers/productRouter.ts
@@ -4,15 +4,15 @@ import { Product, ProductModel } from '../models/productModel'
 
 export const productRouter = express.Router()
 
-productRouter.get('/', expressAsyncHandler(async(req, res) => {
+productRouter.get('/', expressAsyncHandler(async(req,res) => {
     const products = await ProductModel.find()
     res.json(products)
   })
 )
 
-productRouter.get('/:slug',
+productRouter.get('/slug/:slug',
   expressAsyncHandler(async(req,res) => {
-    const product = await ProductModel.find({ slug: req.params.slug })
+    const product = await ProductModel.findOne({ slug: req.params.slug })
     if(product) res.json(product)
     else res.status(404).json({ messsage: 'Product Not Found' })
   })
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { useContext, useEffect } from 'react'
-import { Button, Container, Nav, Navbar } from 'react-bootstrap'
-import { Outlet } from 'react-router-dom'
+import { Badge, Button, Container, Nav, Navbar } from 'react-bootstrap'
+import { Link, Outlet } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Store } from './Store'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
-  const { state: { mode }, dispatch } = useContext(Store)
+  const { state: { mode, cart }, dispatch } = useContext(Store)
 
   useEffect(() => {
     document.body.setAttribute('data-bs-theme', mode)
@@ -17,18 +19,28 @@ function App() {
   return (
     <>
       <div className='d-flex flex-column vh-100'>
+        <ToastContainer position='bottom-center' limit={1} />
         <header>
           <Navbar expand='lg'>
-            <Container>
-              <Navbar.Brand>TS Amazona</Navbar.Brand>
-              <Nav>
-                <Button variant={mode} onClick={switchModeHandler}>
-                  <i className={mode === 'ligth' ? 'fa fa-sun' : 'fa fa-moon' }></i>
-                </Button>
-                <a href='/cart' className='nav-link'>Cart</a>
-                <a href='/signin' className='nav-link'>Sign In</a>
-              </Nav>
+             <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>TS Amazona</Navbar.Brand>
+              </LinkContainer>
             </Container>
+            <Nav>
+              <Button variant={mode} onClick={switchModeHandler}>
+                <i className={mode === 'ligth' ? 'fa fa-sun' : 'fa fa-moon' }></i>
+              </Button>
+              <Link to='/cart' className='nav-link'>
+                Cart
+                { cart.cartItems.length > 0 && (
+                  <Badge pill bg='danger'>
+                    { cart.cartItems.reduce((a,acc) => a + acc.quantity, 0) }
+                  </Badge>
+                )}
+              </Link>
+              <Link to='/signin' className='nav-link'>Sign In</Link>
+            </Nav>
           </Navbar>
         </header>
         <main>
